8d56ece5a8edc5cfa614a22583e490059eabc705
agustin-penas <Tinagus5>
2024-08-08 19:34:42 -0300

    Sumamos rotacion arriba y abajo

--- www/blink-experiment-2.js
+++ www/blink-experiment-2.js
@@ -1,6 +1,6 @@
 const jsPsych = initJsPsych({
-  on_finish: function() {
-		jsPsych.data.displayData();
+  on_finish: function () {
+    jsPsych.data.displayData();
     jsPsych.data.get().localSave(
       'csv',
       `blink-experiment-expose-points${(new Date).toISOString()}.csv`
@@ -8,146 +8,153 @@ const jsPsych = initJsPsych({
   },
   extensions: [{ type: jsPsychExtensionWebgazer }],
 });
- 
- // Define the experiment timeline
-  var timeline = [];
 
-  // Define the parameters of the trial
-  var blink_duration = 1000; // milliseconds
-  var dot_duration = 2000; // milliseconds
-  var trial_duration = 6000; // milliseconds
-  var dot_color = "red";
-	var dot_blink_color = "green";
-  var correct_response = " ";// older browsers return "Spacebar"
+// Define the experiment timeline
+var timeline = [];
 
-  // Create the dot stimulus
-  var dot_html = '<div style="width:50px;height:50px;border-radius:50%;background-color:' + dot_color + ';"></div>';
-  var dot_green_html = '<div style="width:50px;height:50px;border-radius:50%;background-color:' + dot_blink_color + ';"></div>';
-  
-  /*timeline.push({
-    type: jsPsychInitializeCamera
-  });*/
-	timeline.push({
-    type: jsPsychWebgazerInitCamera
-  });
-	timeline.push({
-    type: rastocJSPsych.EventsTrackingStart
-  });
-	timeline.push({
-    type: jsPsychFullscreen
-	});
-	timeline.push(
-		rastocJSPsych.ensureCalibration({
-			performValidation: true,
-			forceCalibration: true,
-			maxRetries: 0,
-		})
-  );
+// Define the parameters of the trial
+var blink_duration = 1000; // milliseconds
+var dot_duration = 2000; // milliseconds
+var trial_duration = 6000; // milliseconds
+var dot_color = "red";
+var dot_blink_color = "green";
+var correct_response = " ";// older browsers return "Spacebar"
 
-	timeline.push({
-    type: jsPsychHtmlButtonResponse,
-    stimulus: `
+// Create the dot stimulus
+var dot_html = '<div style="width:50px;height:50px;border-radius:50%;background-color:' + dot_color + ';"></div>';
+var dot_green_html = '<div style="width:50px;height:50px;border-radius:50%;background-color:' + dot_blink_color + ';"></div>';
+
+/*timeline.push({
+  type: jsPsychInitializeCamera
+});*/
+timeline.push({
+  type: jsPsychWebgazerInitCamera
+});
+timeline.push({
+  type: rastocJSPsych.EventsTrackingStart
+});
+timeline.push({
+  type: jsPsychFullscreen
+});
+
+timeline.push({
+  type: jsPsychVirtualChinrest,
+  blindspot_reps: 3,
+  resize_units: "none"
+});
+
+timeline.push(
+  rastocJSPsych.ensureCalibration({
+    performValidation: true,
+    forceCalibration: true,
+    maxRetries: 1,
+  })
+);
+
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
     <p>
-    In the next experiment you have to:<br>
-      1. Blink five times in quick succession.<br>
-      2. Wait 2 seconds.<br>
-      3. Blink five times in quick succession. <br>
-    You have 20 seconds, if finish early hit the spacebar<br>
-    Press <i>continue</i> to start experiment.<br>
+    En el proximo experimento usted debe:<br>
+      1. Pestañear 5 veces rapido.<br>
+      2. Esperar 2 segundos.<br>
+      3. Pestañear 5 veces rapido. <br>
+    Tiene 20 segundos, si termina antes puede apretar el boton <i>continuar</i>.<br>
+    Presione <i>continuar</i> para iniciar el experimento.<br>
     </p>
     `,
-    choices: ["continue"],
-  })
+  choices: ["continuar"],
+})
 
-	timeline.push({
-		type: jsPsychHtmlKeyboardResponse,
-		choices: " ",
-		stimulus: dot_html,
-		extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
-		trial_duration: 20000,
-	})
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: dot_html,
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
 
-  timeline.push({
-    type: jsPsychHtmlButtonResponse,
-    stimulus: `
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
     <p>
-    In the next experiment you have to:<br>
-      1. Blink five times in a row in a relaxed manner.<br>
-      2. Wait 2 seconds.<br>
-      3. Blink five times in a row in a relaxed manner. <br>
-    You have 20 seconds, if finish early hit the spacebar<br>
-    Press <i>continue</i> to start experiment.<br>
+    En el proximo experimento usted debe:<br>
+      1. Pestañear 5 veces de manera relajada.<br>
+      2. Esperar 2 segundos.<br>
+      3. Pestañear 5 veces de manera relajada.<br>
+    Tiene 20 segundos, si termina antes puede apretar el boton <i>continuar</i>.<br>
+    Presione <i>continuar</i> para iniciar el experimento.<br>
     </p>
     `,
-    choices: ["continue"],
-  })
+  choices: ["continuar"],
+})
 
-	timeline.push({
-		type: jsPsychHtmlKeyboardResponse,
-		choices: " ",
-		stimulus: dot_html,
-		extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
-		trial_duration: 20000,
-	})
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: dot_html,
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
 
-  timeline.push({
-    type: jsPsychHtmlButtonResponse,
-    stimulus: `
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
     <p>
-    In the next experiment you have to:<br>
-      1. Blink for 1 second.<br>
-      2. Wait 2 seconds.<br>
-      3. Blink for 1 second.  <br>
-    You have 20 seconds, if finish early hit the spacebar<br>
-    Press <i>continue</i> to start experiment.<br>
+    En el proximo experimento usted debe:<br>
+      1. Cierre los ojos por 1 segundo.<br>
+      2. Esperar 2 segundos.<br>
+      3. Cierre los ojos por 1 segundo.<br>
+    Tiene 20 segundos, si termina antes puede apretar el boton <i>continuar</i>.<br>
+    Presione <i>continuar</i> para iniciar el experimento.<br>
     </p>
     `,
-    choices: ["continue"],
-  })
+  choices: ["continuar"],
+})
 
-	timeline.push({
-		type: jsPsychHtmlKeyboardResponse,
-		choices: " ",
-		stimulus: dot_html,
-		extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
-		trial_duration: 20000,
-	})
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: dot_html,
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
 
-  timeline.push({
-    type: jsPsychHtmlButtonResponse,
-    stimulus: `
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
     <p>
-    In the next experiment you have to:<br>
-      1. Look at the camera for 10 seconds.<br>
-      2. Look around without head motions for 10 seconds.<br>
-      3. Look around with head motions for 10 seconds.  <br>
-    You have 20 seconds, if finish early hit the spacebar<br>
-    Press <i>continue</i> to start experiment.<br>
+    En el proximo experimento usted debe:<br>
+      1. Mirar fijamente a la camara pestañeando 10 veces.<br>
+      2. Mueva la mirada por toda la pantalla sin movimientos de cabeza pestañeando 10 veces.<br>
+      3. Mueva la mirada por toda la pantalla con movimientos de la cabeza pestañeando 10 veces.  <br>
+    Tiene 20 segundos, si termina antes puede apretar el boton <i>continuar</i>.<br>
+    Presione <i>continuar</i> para iniciar el experimento.<br>
     </p>
     `,
-    choices: ["continue"],
-  })
+  choices: ["continuar"],
+})
 
-	timeline.push({
-		type: jsPsychHtmlKeyboardResponse,
-		choices: " ",
-		stimulus: dot_html,
-		extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
-		trial_duration: 32000,
-	})
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: dot_html,
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 32000,
+})
 
-	timeline.push({
-    type: jsPsychHtmlButtonResponse,
-    stimulus: `
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
     <p>
-      Fin del experimento. Presioná <i>finalizar</i> y esperá unos segundos a
-      que la pantalla quede en blanco. Luego podés cerrar la pestaña. <br>
+      Fin del experimento. Presione <i>finalizar</i> y espere unos segundos a
+      que la pantalla quede en blanco. Luego podra cerrar la pestaña. <br>
       Muchas gracias por participar c:
     </p>
     `,
-    choices: ["finalizar"],
-  })
-	timeline.push({ type: rastocJSPsych.EventsTrackingStop });
+  choices: ["finalizar"],
+})
+timeline.push({ type: rastocJSPsych.EventsTrackingStop });
 
-  // Start the experiment
-	jsPsych.run(timeline);
+// Start the experiment
+jsPsych.run(timeline);
--- www/rotation-experiment.js
+++ www/rotation-experiment.js
@@ -1,6 +1,6 @@
 const jsPsych = initJsPsych({
-  on_finish: function() {
-		jsPsych.data.displayData();
+  on_finish: function () {
+    jsPsych.data.displayData();
     jsPsych.data.get().localSave(
       'csv',
       `rotation-experiment-expose-points${(new Date).toISOString()}.csv`
@@ -8,112 +8,178 @@ const jsPsych = initJsPsych({
   },
   extensions: [{ type: jsPsychExtensionWebgazer }],
 });
- 
- // Define the experiment timeline
-  var timeline = [];
 
-  // Define the parameters of the trial
-  var blink_duration = 1000; // milliseconds
-  var dot_duration = 2000; // milliseconds
-  var trial_duration = 6000; // milliseconds
-  var dot_color = "red";
-	var dot_blink_color = "green";
-  var correct_response = " ";// older browsers return "Spacebar"
+// Define the experiment timeline
+var timeline = [];
 
-  // Create the dot stimulus
-  var dot_html = '<div style="width:50px;height:50px;border-radius:50%;background-color:' + dot_color + ';"></div>';
-  var dot_green_html = '<div style="width:50px;height:50px;border-radius:50%;background-color:' + dot_blink_color + ';"></div>';
-  
+// Define the parameters of the trial
+var blink_duration = 1000; // milliseconds
+var dot_duration = 2000; // milliseconds
+var trial_duration = 6000; // milliseconds
+var dot_color = "red";
+var dot_blink_color = "green";
+var correct_response = " ";// older browsers return "Spacebar"
 
-	timeline.push({
-    type: jsPsychWebgazerInitCamera
-  });
-	timeline.push({
-    type: rastocJSPsych.EventsTrackingStart
-  });
-	timeline.push({
-    type: jsPsychFullscreen
-	});
-	/*timeline.push(
-		rastocJSPsych.ensureCalibration({
-			performValidation: true,
-			forceCalibration: true,
-			maxRetries: 0,
-		})
-  );*/
+// Create the dot stimulus
+var dot_html = '<div style="width:50px;height:50px;border-radius:50%;background-color:' + dot_color + ';"></div>';
+var dot_green_html = '<div style="width:50px;height:50px;border-radius:50%;background-color:' + dot_blink_color + ';"></div>';
 
-	timeline.push({
-    type: jsPsychVideoButtonResponse,
-    stimulus: ['video/derecha_hombro.mp4'],
-    choices: ["continuar"],
-    prompt: "<p>Luego de apretar el boton continuar gire la cabeza hacia su hombro derecho como muestra el video.</p>",
+
+timeline.push({
+  type: jsPsychWebgazerInitCamera
+});
+timeline.push({
+  type: rastocJSPsych.EventsTrackingStart
+});
+timeline.push({
+  type: jsPsychFullscreen
+});
+/*timeline.push(
+  rastocJSPsych.ensureCalibration({
+    performValidation: true,
+    forceCalibration: true,
+    maxRetries: 0,
   })
+);*/
+timeline.push({
+  type: jsPsychVirtualChinrest,
+  blindspot_reps: 3,
+  resize_units: "none"
+});
 
-	timeline.push({
-		type: jsPsychHtmlKeyboardResponse,
-		choices: " ",
-		stimulus: dot_html,
-		extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
-		trial_duration: 20000,
-	})
+timeline.push({
+  type: jsPsychVideoButtonResponse,
+  stimulus: ['video/derecha_hombro.mp4'],
+  choices: ["continuar"],
+  prompt: "<p>Luego de apretar el boton <i>continuar</i> gire la cabeza hacia su hombro derecho como muestra el video.</p>",
+})
 
-	timeline.push({
-    type: jsPsychVideoButtonResponse,
-    stimulus: ['video/derecha.mp4'],
-    choices: ["continuar"],
-    prompt: "<p>Luego de apretar el boton continuar gire la cabeza hacia la derecha como muestra el video.</p>",
-  })
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: " ",
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
 
-	timeline.push({
-		type: jsPsychHtmlKeyboardResponse,
-		choices: " ",
-		stimulus: dot_html,
-		extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
-		trial_duration: 20000,
-	})
+timeline.push({
+  type: jsPsychVideoButtonResponse,
+  stimulus: ['video/derecha.mp4'],
+  choices: ["continuar"],
+  prompt: "<p>Luego de apretar el boton <i>continuar</i> gire la cabeza hacia la derecha, a medio camino entre mirar hacia el frente y hacia el hombro, como muestra el video.</p>",
+})
 
-	timeline.push({
-    type: jsPsychVideoButtonResponse,
-    stimulus: ['video/izquierda_hombro.mp4'],
-    choices: ["continuar"],
-    prompt: "<p>Luego de apretar el boton continuar gire la cabeza hacia el hombro izquierdo como muestra el video.</p>",
-  })
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: " ",
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
 
-	timeline.push({
-		type: jsPsychHtmlKeyboardResponse,
-		choices: " ",
-		stimulus: dot_html,
-		extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
-		trial_duration: 20000,
-	})
+timeline.push({
+  type: jsPsychVideoButtonResponse,
+  stimulus: ['video/izquierda_hombro.mp4'],
+  choices: ["continuar"],
+  prompt: "<p>Luego de apretar el boton <i>continuar</i> gire la cabeza hacia el hombro izquierdo como muestra el video.</p>",
+})
 
-	timeline.push({
-    type: jsPsychVideoButtonResponse,
-    stimulus: ['video/izquierda.mp4'],
-    choices: ["continuar"],
-    prompt: "<p>Luego de apretar el boton continuar gire la cabeza hacia la izquierda como muestra el video.</p>",
-  })
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: " ",
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
 
-	timeline.push({
-		type: jsPsychHtmlKeyboardResponse,
-		choices: " ",
-		stimulus: dot_html,
-		extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
-		trial_duration: 20000,
-	})
+timeline.push({
+  type: jsPsychVideoButtonResponse,
+  stimulus: ['video/izquierda.mp4'],
+  choices: ["continuar"],
+  prompt: "<p>Luego de apretar el boton <i>continuar</i> gire la cabeza hacia la izquierda, a medio camino entre mirar hacia el frente y hacia el hombro, como muestra el video.</p>",
+})
 
-	timeline.push({
-    type: jsPsychHtmlButtonResponse,
-    stimulus: `
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: " ",
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
+
+timeline.push({
+  type: jsPsychVideoButtonResponse,
+  stimulus: ['video/arriba.mp4'],
+  choices: ["continuar"],
+  prompt: "<p>Luego de apretar el boton <i>continuar</i> gire la cabeza mirando al techo como muestra el video.</p>",
+})
+
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: " ",
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
+
+timeline.push({
+  type: jsPsychVideoButtonResponse,
+  stimulus: ['video/arriba2.mp4'],
+  choices: ["continuar"],
+  prompt: "<p>Luego de apretar el boton <i>continuar</i> gire la cabeza, fijando la mirada por encima del monitor, como muestra el video.</p>",
+})
+
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: " ",
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
+
+timeline.push({
+  type: jsPsychVideoButtonResponse,
+  stimulus: ['video/abajo.mp4'],
+  choices: ["continuar"],
+  prompt: "<p>Luego de apretar el boton <i>continuar</i> gire la cabeza mirando su ombligo como muestra el video.</p>",
+})
+
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: " ",
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
+
+timeline.push({
+  type: jsPsychVideoButtonResponse,
+  stimulus: ['video/abajo2.mp4'],
+  choices: ["continuar"],
+  prompt: "<p>Luego de apretar el boton <i>continuar</i> gire la cabeza mirando su ombligo como muestra el video.</p>",
+})
+
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  choices: ["continuar"],
+  stimulus: " ",
+  extensions: [{ type: jsPsychExtensionWebgazer, params: { targets: [] } }],
+  trial_duration: 20000,
+})
+
+timeline.push({
+  type: jsPsychHtmlButtonResponse,
+  stimulus: `
     <p>
       Fin del experimento. Presioná <i>finalizar</i> y esperá unos segundos a
       que la pantalla quede en blanco. Luego podés cerrar la pestaña. <br>
       Muchas gracias por participar c:
     </p>
     `,
-    choices: ["finalizar"],
-  })
-	timeline.push({ type: rastocJSPsych.EventsTrackingStop });
+  choices: ["finalizar"],
+})
 
-  // Start the experiment
-	jsPsych.run(timeline);
+timeline.push({ type: rastocJSPsych.EventsTrackingStop });
+
+// Start the experiment
+jsPsych.run(timeline);
